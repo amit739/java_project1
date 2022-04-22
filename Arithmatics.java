71140bf0f7090c6b529a47f329b2c20ae236435a
amit_zzz <39086709+amit739@users.noreply.github.com>
2022-04-22 13:38:57 +0530

    Add files via upload

---
+++ Arithmatics.java
@@ -0,0 +1,18 @@
+
+public class Arithmatics {
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 5;
+		int add = num1 + num2;
+		int sub = num1 - num2;
+		int mult = num1 * num2;
+		int div = num1 / num2;
+		int rem = num1 % num2;
+		System.out.println("addition is=" +add );
+		System.out.println("subtraction is=" +sub);
+		System.out.println("multiplication is=" +mult);
+		System.out.println("division is=" +div);
+		System.out.println("remainder is=" +rem);
+		
+	}
+}
